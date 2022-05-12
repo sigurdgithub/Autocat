bd52a397163a859a5d88066aea7139c57786d980
LiesbethPoelmans <liesbeth.poelmans@gmail.com>
2022-05-12 23:01:40 +0200

    Dashboards: HTML / CSS

--- autocat/resources/views/fosterDashboard.blade.php
+++ autocat/resources/views/fosterDashboard.blade.php
@@ -8,4 +8,74 @@
                 </span> Dashboard        
             </h3>
         </div>
+        <div class="grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Meldingen</h4>
+                    </p>
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                            <th>Kat</th>
+                            <th>Type melding</th>
+                            <th>Bericht</th>
+                            <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Malou</td>
+                                <td>Profiel up to date?</td>
+                                <td>lorem ipsum dolor sit amet</td>
+                                <td>                          
+                                    <button type="button" class="btn btn-inverse-info btn-icon">
+                                        <i class="mdi mdi-delete"></i>
+                                    </button>
+                                </td>                      
+                            </tr>
+                            <tr>
+                                <td>Caramel</td>
+                                <td>Vraag voor opvang</td>
+                                <td>lorem ipsum dolor sit amet</td>
+                                <td>                          
+                                    <button type="button" class="btn btn-inverse-info btn-icon">
+                                        <i class="mdi mdi-delete"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Tux</td>
+                                <td>Afspraak adoptant</td>
+                                <td>lorem ipsum dolor sit amet</td>
+                                <td>                          
+                                    <button type="button" class="btn btn-inverse-info btn-icon">
+                                        <i class="mdi mdi-delete"></i>
+                                    </button>
+                                </td>                    
+                            </tr>
+                            <tr>
+                                <td>Pumpkin</td>
+                                <td>Adoptant goedgekeurd</td>
+                                <td>lorem ipsum dolor sit amet</td>
+                                <td>                          
+                                    <button type="button" class="btn btn-inverse-info btn-icon">
+                                        <i class="mdi mdi-delete"></i>
+                                    </button>
+                                </td>                    
+                            </tr>
+                            <tr>
+                                <td>Olijfje</td>
+                                <td>Vraag voor opvang</td>
+                                <td>lorem ipsum dolor sit amet</td>
+                                <td>                          
+                                    <button type="button" class="btn btn-inverse-info btn-icon">
+                                        <i class="mdi mdi-delete"></i>
+                                    </button>
+                                </td>                    
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>        
     @endsection
--- autocat/resources/views/shelterDashboard.blade.php
+++ autocat/resources/views/shelterDashboard.blade.php
@@ -8,4 +8,80 @@
                 </span> Dashboard        
             </h3>
         </div>
+        <div class="grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Meldingen</h4>
+                    </p>
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                            <th>Pleeggezin</th>
+                            <th>kat</th>
+                            <th>Type melding</th>
+                            <th>Bericht</th>
+                            <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Liesbeth P</td>
+                                <td>Malou</td>
+                                <td>Profiel up to date?</td>
+                                <td>lorem ipsum dolor sit amet</td>
+                                <td>                          
+                                    <button type="button" class="btn btn-inverse-success btn-icon">
+                                        <i class="mdi mdi-delete"></i>
+                                    </button>
+                                </td>                      
+                            </tr>
+                            <tr>
+                                <td>Sophie C</td>
+                                <td>Caramel</td>
+                                <td>Weging</td>
+                                <td>lorem ipsum dolor sit amet</td>
+                                <td>                          
+                                    <button type="button" class="btn btn-inverse-success btn-icon">
+                                        <i class="mdi mdi-delete"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Leentje B</td>
+                                <td>Tux</td>
+                                <td>Opvang geweigerd</td>
+                                <td>lorem ipsum dolor sit amet</td>
+                                <td>                          
+                                    <button type="button" class="btn btn-inverse-success btn-icon">
+                                        <i class="mdi mdi-delete"></i>
+                                    </button>
+                                </td>                    
+                            </tr>
+                            <tr>
+                                <td>Doenja S</td>
+                                <td>Pumpkin</td>
+                                <td>Afspraak adoptant</td>
+                                <td>lorem ipsum dolor sit amet</td>
+                                <td>                          
+                                    <button type="button" class="btn btn-inverse-success btn-icon">
+                                        <i class="mdi mdi-delete"></i>
+                                    </button>
+                                </td>                    
+                            </tr>
+                            <tr>
+                                <td>Jan V</td>
+                                <td>Olijfje</td>
+                                <td>Opvang geaccepteerd</td>
+                                <td>lorem ipsum dolor sit amet</td>
+                                <td>                          
+                                    <button type="button" class="btn btn-inverse-success btn-icon">
+                                        <i class="mdi mdi-delete"></i>
+                                    </button>
+                                </td>                    
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div> 
     @endsection
