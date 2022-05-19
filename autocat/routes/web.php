393fe5e1d1138a331ff8bcba303b5f9e72a5e5ad
Sigurd B <sigurdboels96@hotmail.com>
2022-05-19 14:29:44 +0200

    laravel breeze install

--- autocat/routes/web.php
+++ autocat/routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\NotificationController;
-use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,48 +12,13 @@ use App\Http\Controllers\LoginController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// Login
-Route::post('custom-login', [LoginController::class, 'customLogin'])->name('login.custom');
+
 Route::get('/', function () {
-    return view('login');
-});
-
-Route::get('/katDetail', function () {
-    return view('catDetail');
-});
-
-Route::get('/kattenOverzicht', function () {
-    return view('catOverview');
-});
-
-Route::get('/pleeggezinAccount', function () {
-    return view('fosterAccount');
-});
-
-Route::get('/pleeggezinDashboard', function () {
-    return view('fosterDashboard');
-});
-
-Route::get('/pleeggezinnenOverzicht', function () {
-    return view('fosterOverview');
-});
-
-
-Route::get('/asielDashboard', function () {
-    return view('shelterDashboard');
-});
-
-Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/privacy', function () {
-    return view('privacy');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/notifications/{fosterId}', [NotificationController::class, 'showByFosterId'])->name('notifications');
-Route::get('/asielDashboard', [NotificationController::class, 'showShelterNotifications'])->name('shelterNotifications');
-Route::get('/asielDashboard/ajax/{fosterId}', [NotificationController::class, 'getCatsByFosterId']);
-Route::delete('/notifications_delete/{id}', [NotificationController::class, 'delete'])->name('delete');
-Route::post('/addNotification', [NotificationController::class, 'store'])->name('addNotification');
-Route::post('/addNotificationShelter', [NotificationController::class, 'storeShelter'])->name('addNotificationShelter');
+require __DIR__.'/auth.php';
