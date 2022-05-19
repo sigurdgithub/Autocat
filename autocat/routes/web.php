8f5d7c408b65b2f5bd70eb572b1cc7ce0f644d41
Jari Boeckstaens <jboeckstaens@gmail.com>
2022-05-19 16:26:12 +0200

    Reset routes

--- autocat/routes/web.php
+++ autocat/routes/web.php
@@ -24,10 +24,45 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/katDetail', function () {
+    return view('catDetail');
+});
+
+Route::get('/kattenOverzicht', function () {
+    return view('catOverview');
+});
+
+Route::get('/pleeggezinAccount', function () {
+    return view('fosterAccount');
+});
+
+Route::get('/pleeggezinDashboard', function () {
+    return view('fosterDashboard');
+});
+
+Route::get('/pleeggezinnenOverzicht', function () {
+    return view('fosterOverview');
+});
+
+
+Route::get('/asielDashboard', function () {
+    return view('shelterDashboard');
+});
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/privacy', function () {
+    return view('privacy');
+});
+
 Route::get('/notifications/{fosterId}', [DashBoardController::class, 'showByFosterId'])->name('notifications');
 Route::get('/asielDashboard', [DashBoardController::class, 'showShelterNotifications'])->name('shelterNotifications');
 Route::get('/asielDashboard/ajax/{fosterId}', [DashBoardController::class, 'getCatsByFosterId']);
 Route::delete('/notifications_delete/{id}', [DashBoardController::class, 'delete'])->name('delete');
 Route::post('/addNotification', [DashBoardController::class, 'store'])->name('addNotification');
 Route::post('/addNotificationShelter', [DashBoardController::class, 'storeShelter'])->name('addNotificationShelter');
+
+
 require __DIR__.'/auth.php';
