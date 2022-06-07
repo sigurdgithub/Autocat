25bcd53bd00b7721bb87f6137763a0436331f45a
LiesbethPoelmans <liesbeth.poelmans@gmail.com>
2022-06-07 15:53:16 +0200

    Pets & roommates: models & migrations

---
+++ autocat/database/migrations/2022_05_29_153052_create_roommates.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roommates', function (Blueprint $table) {
+            $table->id();
+            $table->integer('fosterFamily_id');
+            $table->date('relation');
+            $table->integer('dateOfBirth');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roommates');
+    }
+};
