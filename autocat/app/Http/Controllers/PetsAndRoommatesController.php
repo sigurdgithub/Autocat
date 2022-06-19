94d925a4fb704963a8f3d4fe38f9d702bae29dd3
Sigurd B <sigurdboels96@hotmail.com>
2022-06-19 22:59:33 +0200

    Roomates & Pets Encryption Bugfix

--- autocat/app/Http/Controllers/PetsAndRoommatesController.php
+++ autocat/app/Http/Controllers/PetsAndRoommatesController.php
@@ -7,70 +7,81 @@ use App\Models\FosterFamily;
 use App\Models\Pet;
 use App\Models\Roommate;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Crypt;
 
 
 class PetsAndRoommatesController extends Controller
 {
     public static function showPetsByFosterId($fosterId)
     {
-        $matchCase = ['fosterFamily_id'=> $fosterId];
+        $matchCase = ['fosterFamily_id' => $fosterId];
         return Pet::where($matchCase)->get();
     }
 
     public static function showRoommatesByFosterId($fosterId)
     {
-        $matchCase = ['fosterFamily_id'=> $fosterId];
+        $matchCase = ['fosterFamily_id' => $fosterId];
         return Roommate::where($matchCase)->get();
     }
 
-    public function deletePet($id) {
+    public function deletePet($id)
+    {
         $pet = Pet::find($id);
         //Get fosterFamily Id
         $fosterFamily_id = $pet->fosterFamily_id;
         $pet->delete();
-        return redirect()->route('fosterAccount', $fosterFamily_id);
+        $foster_id_crypt = Crypt::encryptString($fosterFamily_id);
+        return redirect()->route('fosterAccount', $foster_id_crypt);
     }
 
-    public function deleteRoommate($id) {
+    public function deleteRoommate($id)
+    {
         $roommate = Roommate::find($id);
         //Get fosterFamily Id
         $fosterFamily_id = $roommate->fosterFamily_id;
         $roommate->delete();
-        return redirect()->route('fosterAccount', $fosterFamily_id);
+        $foster_id_crypt = Crypt::encryptString($fosterFamily_id);
+        return redirect()->route('fosterAccount', $foster_id_crypt);
     }
 
     public function storePet(Request $request)
     {   //Validate
         $validation = $request->validate([
-            'species'=> 'required',
-            'dateOfBirthPet'=>'required',
+            'species' => 'required',
+            'dateOfBirthPet' => 'required',
         ]);
 
         //Store
         $pet = Pet::firstOrCreate(
-            ['fosterFamily_id' => $request->input('fosterFamily_id'),
-            'species' => $request->input('species'),
-            'dateOfBirth' => $request->input('dateOfBirthPet'),
-            ]);
-        
-        return redirect()->route('fosterAccount', $request->input('fosterFamily_id'));
+            [
+                'fosterFamily_id' => $request->input('fosterFamily_id'),
+                'species' => $request->input('species'),
+                'dateOfBirth' => $request->input('dateOfBirthPet'),
+            ]
+        );
+        $foster_id_crypt = Crypt::encryptString(auth()->user()->fosterFamily_id);
+        return redirect()->route('fosterAccount', $foster_id_crypt);
     }
 
     public function storeRoommate(Request $request)
     {   //Validate    
-        /* dd($request); */        
+        /* dd($request); */
         $validation = $request->validate([
-            'relation'=> 'required',
-            'dateOfBirthRoommate'=>'required',
+            'relation' => 'required',
+            'dateOfBirthRoommate' => 'required',
         ]);
-        
+
         //Store
+
         $roommate = Roommate::firstOrCreate(
-            ['fosterFamily_id' => $request->input('fosterFamily_id'),
-            'relation' => $request->input('relation'),
-            'dateOfBirth' => $request->input('dateOfBirthRoommate'),
-            ]);
-        
-        return redirect()->route('fosterAccount', $request->input('fosterFamily_id'));
+            [
+                'fosterFamily_id' => $request->input('fosterFamily_id'),
+                'relation' => $request->input('relation'),
+                'dateOfBirth' => $request->input('dateOfBirthRoommate'),
+            ]
+        );
+        $foster_id_crypt = Crypt::encryptString(auth()->user()->fosterFamily_id);
+
+        return redirect()->route('fosterAccount', $foster_id_crypt);
     }
 }
